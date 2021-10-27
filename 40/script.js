d86f0f2788eae1e3259acaa7d3d09c51be574725
VNagorniy <vnagorniyrnd@gmail.com>
2021-10-27 19:29:51 +0300

    Знакомство с Data

---
+++ 40/script.js
@@ -0,0 +1,28 @@
+const now = new Date('2021-27-10');
+new Date.parse('2021-27-10')
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+console.log(now.setHours(18));
+console.log(now);
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+let start = new Date();
+
+for(let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} миллисекунд`)
