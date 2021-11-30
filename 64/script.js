d581e96a28a11c7671ce43632974b1dc122cb77d
VNagorniy <vnagorniyrnd@gmail.com>
2021-11-30 19:11:56 +0300

    Регулярные выражения

---
+++ 64/script.js
@@ -0,0 +1,24 @@
+// new RegExp('pattern', 'flags');
+
+// /pattern/f
+
+// const ans = prompt('Введите ваше имя');
+
+// const reg = /n/gi;
+
+// console.log(reg.test(ans));
+
+const ans = prompt('Введите ваше число');
+
+const reg = /\d/;
+
+console.log(ans.match(reg));
+
+// console.log(ans.search(reg));
+
+// console.log(ans.match(reg));
+
+// const pass = prompt('Password');
+
+// console.log(pass.replace(/./g, '*'));
+// console.log('12-34-56'.replace(/-/g, ':'));
